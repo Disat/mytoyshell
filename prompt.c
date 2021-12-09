3c80db9f4e6e6f44323a51c6949d9923084f8fcc
vhxn <wszxplayers@gmail.com>
2021-12-09 01:40:50 -0800

    inital project

---
+++ prompt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "shell.h"
+
+void print_promptl(void){
+	fprintf(stderr, "$ ");
+}
+
+void print_prompt2(void){
+	fprintf(stderr, "> ");
+}
---
+++ shell.h
@@ -0,0 +1,8 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+void print_promptl(void);
+void print_prompt2(void);
+
+char *read_cmd(void);
+#endif
